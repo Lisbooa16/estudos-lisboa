b1a4baae4309ad7825b296153ac374256a24a1e7
Guilherme Lisboa <guibrbr161@gmail.com>
2023-01-25 22:04:16 -0300

    up

--- secao03/main.py
+++ secao03/main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI, HTTPException, status
+from typing import Optional
+from fastapi import FastAPI, HTTPException, status, Response
+from models import Curso
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
@@ -29,6 +32,35 @@ async def get_curso(curso_id: int):
     except KeyError:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Curso não encontrado")
     return curso
+
+@app.post('/cursos', status_code=status.HTTP_201_CREATED)
+async def post_curso(curso: Curso):
+    next_id: int = len(cursos) + 1
+    cursos[next_id] = curso
+    del curso.id 
+    return curso
+
+
+@app.put("/cursos/{curso_id}")
+async def put_curso(curso_id: int, curso: Curso):
+    if curso_id in cursos:
+        cursos[curso_id] =  curso
+        del curso.id 
+        return curso
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Não existe um curso com id {curso_id}")
+
+
+@app.delete("/cursos/{curso_id}")
+async def delete_curso(curso_id: int):
+    if curso_id in cursos:
+        del cursos[curso_id]
+        # return JSONResponse(status_code=status.HTTP_204_NO_CONTENT)
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Não existe um curso com id {curso_id}")
+
+
 
 if __name__ == '__main__':
     import uvicorn
