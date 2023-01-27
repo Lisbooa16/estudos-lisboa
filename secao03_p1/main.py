34250224dcd9edf314a3056da3ff9fe4d4e44217
guilherme lisboa <guilherme.lisboa@deltaux.com.br>
2023-01-27 11:21:35 -0300

    criacao primeira api com o banco de dados

--- secao03_p1/main.py
+++ secao03_p1/main.py
@@ -17,6 +17,7 @@ def fake_db():
 
 app = FastAPI(title='API de Cursos da Geek University', version="0.0.1", description="Uma API para estudo do FastAPI")
 
+
 @app.get('/cursos', description="Retorna todos os cursos ou uma lista vazia.", summary="Retorna todos os cursos", response_model=List[Curso], response_description="Cursos encontrados com sucesso")
 async def get_cursos(db: Any = Depends(fake_db)):
     return cursos
@@ -29,6 +30,7 @@ async def get_curso(curso_id: int = Path(default=None, title='ID do curso', desc
     except KeyError:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Curso não encontrado")
     return curso
+
 
 @app.post('/cursos', status_code=status.HTTP_201_CREATED, description="Inserir um novo dado para lista de cursos", summary="Inserir novo dado")
 async def post_curso(curso: Curso, db: Any = Depends(fake_db)):
